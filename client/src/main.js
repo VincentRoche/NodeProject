6deb76a7177df618b645ef9dcd7a9783412d76a3
VincentRoche <vin.roche@hotmail.fr>
2019-12-04 16:04:16 +0100

    Added vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+
+import HelloWorld from './components/HelloWorld'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '*', component: HelloWorld }
+  ]
+})
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
