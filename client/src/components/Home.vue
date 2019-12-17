c8068ff39146190e268bb033ef70503905b82318
VincentRoche <vincent.roche@efrei.net>
2019-12-18 00:13:07 +0100

    Join or create a game from the home page if logged in

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,28 +1,88 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+  <v-container
+    class="mt-12"
+    fluid
+    text-center
+  >
 
-      <v-flex mb-4>
+    <v-row justify="center">
+      <v-col md="8">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to EPIC JUSTE PRIX 3000
         </h1>
-        <div class="my-2">
-          <v-btn large color="primary" to="/login">Log in</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn large color="primary" to="/newAccount">Create an account</v-btn>
-        </div>
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
+
+    <template v-if="!this.$store.getters['session/isLoggedIn']">
+      <v-row justify="center">
+        <v-col sm="4" lg="2">
+          <v-btn large block color="primary" to="/login">Log in</v-btn>
+        </v-col>
+        <v-col sm="4" lg="2">
+          <v-btn large block color="primary" to="/newAccount">Create an account</v-btn>
+        </v-col>
+      </v-row>
+    </template>
+
+    <template v-else>
+      <v-row justify="center">
+        <v-col sm="6" md="4" lg="2">
+          <p class="font-weight-medium">Join a game:</p>
+          <v-form>
+            <v-text-field
+              v-model="gameNumber"
+              label="Game number"
+              solo
+              required
+              type="number"
+              :disabled="joinLoading"
+            >
+              <template v-slot:append>
+                <v-btn 
+                  color="primary" 
+                  fab
+                  small
+                  @click="joinGame"
+                  :loading="joinLoading"
+                >
+                  <v-icon>mdi-send</v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
+          </v-form>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col>
+          <v-btn
+            to="/lobby"
+            :disabled="joinLoading"
+          >
+            Create a game
+          </v-btn>
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
+    gameNumber: '',
+    joinLoading: false
   }),
-};
+  methods: {
+    /**
+     * Attempt to join game with the given number
+     */
+    joinGame () {
+      this.joinLoading = true
+      // Faire la requête...
+
+      // Si c'est valide :
+      this.$router.push(`/lobby/${this.gameNumber}`)
+    }
+  }
+}
 </script>
