8417261090c115a1b75f69a686424c683cbbdbba
VincentRoche <vincent.roche@efrei.net>
2019-12-25 23:48:15 +0100

    Added login form validations

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,49 +1,54 @@
 <template>
-  <v-container
-    class="fill-height"
-    fluid
+    <v-container
+        class="fill-height"
+        fluid
     >
     <v-row
         align="center"
         justify="center"
     >
         <v-col
-        cols="12"
-        sm="8"
-        md="4"
+            cols="12"
+            sm="8"
+            md="4"
         >
         <v-card class="elevation-12">
             <v-toolbar
-            color="primary"
-            dark
-            flat
+                color="primary"
+                dark
+                flat
             >
                 <v-toolbar-title>Log in</v-toolbar-title>
             </v-toolbar>
             <v-card-text>
-                <v-form>
+                <v-form
+                    ref="form"
+                    v-model="valid"
+                >
                     <v-text-field
-                    label="Username"
-                    name="username"
-                    prepend-icon="mdi-account"
-                    type="text"
-                    v-model="username"
+                        label="Username"
+                        name="username"
+                        prepend-icon="mdi-account"
+                        type="text"
+                        v-model="username"
+                        :rules="usernameRules"
                     />
 
                     <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    prepend-icon="mdi-lock"
-                    type="password"
-                    v-model="password"
+                        id="password"
+                        label="Password"
+                        name="password"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="password"
+                        :rules="passwordRules"
                     />
                 </v-form>
             </v-card-text>
             <v-card-actions>
                 <v-btn text to="/newAccount">Create an account</v-btn>
                 <v-spacer />
-                <v-btn color="primary" @click="tryToLogin">Log in</v-btn>
+                <v-btn color="primary" @click="tryToLogin" :loading="loading">Log in</v-btn>
             </v-card-actions>
         </v-card>
         </v-col>
@@ -54,25 +59,38 @@
 <script>
 export default {
   data: () => ({
+      valid: true,
+      username: '',
+      usernameRules: [
+        v => !!v || 'Username is required'
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ],
+      loading: false,
+      error: ''
   }),
   methods: {
-      tryToLogin() {
-        // Cette requête pour se connecter te renvoie les messages suivants :
-        // Soit une erreur liée au schema mongoose genre 'Username must not be empty.'
-        // Soit 'User doesn't exist.'
-        // Soit 'User logged in.'
-        // Faut passer à la suite seulement si tu reçois 'User logged in.'
-        // messages présents dans res.data
-        // En sachant qu'à chaque fois je te renvoie un tableau de string
-        // parce que tu peux avoir plusieurs erreurs en même temps
-        // donc si tu veux tu peux faire une sorte de 'liste' à afficher à côté
-        this.axios.post('http://localhost:4000/log', {
-                    username: this.username,
-                    password: this.password
-                })
-                .then(function (res) {
-                    alert(res.data)
-                })
+      async tryToLogin() {
+        if (this.$refs.form.validate()) {
+            // Cette requête pour se connecter te renvoie les messages suivants :
+            // Soit une erreur liée au schema mongoose genre 'Username must not be empty.'
+            // Soit 'User doesn't exist.'
+            // Soit 'User logged in.'
+            // Faut passer à la suite seulement si tu reçois 'User logged in.'
+            // messages présents dans res.data
+            // En sachant qu'à chaque fois je te renvoie un tableau de string
+            // parce que tu peux avoir plusieurs erreurs en même temps
+            // donc si tu veux tu peux faire une sorte de 'liste' à afficher à côté
+            this.axios.post('http://localhost:4000/log', {
+                username: this.username,
+                password: this.password
+            })
+            .then(function (res) {
+                alert(res.data)
+            })
+        }
       }
   }
 };
